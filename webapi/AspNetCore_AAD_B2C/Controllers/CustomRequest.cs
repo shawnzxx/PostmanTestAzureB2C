f6a8e5bcc157ac46b7c3f6bf6e9d6658c40f802e
shawnzxx <shawnzhang.dev@gmail.com>
2019-10-01 01:19:52 +0800

    add cookie support

---
+++ webapi/AspNetCore_AAD_B2C/Controllers/CustomRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCore_AAD_B2C.Controllers
+{
+    public class CustomRequest
+    {
+        public string token { get; set; }
+    }
+
+    public class CustomResponse
+    {
+        public bool valid { get; set; }
+        public string encryptedToken { get; set; }
+    }
+}
--- webapi/AspNetCore_AAD_B2C/Controllers/ValuesController.cs
+++ webapi/AspNetCore_AAD_B2C/Controllers/ValuesController.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AspNetCore_AAD_B2C.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        [HttpPost("cookie")]
+        public HttpResponseMessage Post([FromBody]CustomRequest request)
+        {
+
+            var response = new CustomResponse()
+            {
+                encryptedToken = request.token + "encypted",
+                valid = true
+            };
+            string jsonRes = JsonConvert.SerializeObject(response);
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            //create and set cookie in response
+            var cookie = new CookieHeaderValue("sessionId", "123456");
+            cookie.Expires = DateTimeOffset.Now.AddDays(1);
+            cookie.Domain = Request.Host.Host;
+            cookie.Path = "/";
+
+            //resp.Headers.AddCookies(new CookieHeaderValue[] { cookie });
+            resp.Content = new StringContent("[OK]", System.Text.Encoding.UTF8, "text/plain");
+            return resp;
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var option = new CookieOptions();
+            option.Expires = DateTime.Now.AddMinutes(10);
+            option.Domain = Request.Host.Host;
+            option.Path = "/";
+            option.HttpOnly = true;
+            option.Secure = true;
+            option.SameSite = SameSiteMode.Strict;
+
+            var response = new CustomResponse()
+            {
+                encryptedToken = "encypted",
+                valid = true
+            };
+
+            Response.Cookies.Append("sessionId", "123456", option);
+            return Ok(response);
         }
 
         // GET api/values/5
